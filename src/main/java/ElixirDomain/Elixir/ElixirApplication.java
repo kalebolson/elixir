e7eb4e9235b5ab33311234827937f63ec04c0c8e
Kaleb Olson <kalebroyolson@gmail.com>
2021-04-09 17:05:02 -0500

    Ok NOW spring is set up

---
+++ src/main/java/ElixirDomain/Elixir/ElixirApplication.java
@@ -0,0 +1,13 @@
+package ElixirDomain.Elixir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElixirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElixirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ElixirDomain/Elixir/ElixirApplicationTests.java
@@ -0,0 +1,13 @@
+package ElixirDomain.Elixir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElixirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
